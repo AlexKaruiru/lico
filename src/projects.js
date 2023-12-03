8baaeed994bf9c3569a22163932490cb6df4a28b
AlexKaruiru <alexndegwa49@gmail.com>
2023-12-03 15:40:37 +0300

    added chalenges

--- src/projects.js
+++ src/projects.js
@@ -5,8 +5,14 @@ export const projects = [
       location: 'Nairobi',
       image: require('./images/5.png'),
       about: 'Description about Project 1.',
-      challenges: 'Challenges faced by Project 1.',
-      achievements: 'Achievements of Project 1.',
+      challenges: {
+        image: require('./images/challenge1.png'),
+        description: 'Challenges faced by Project 1.',
+      },
+      achievements: {
+        image: require('./images/achievement1.png'),
+        description: 'Achievements of Project 1.',
+      },
       partners: 'Partners involved in Project 1.',     
     },
     {
@@ -15,8 +21,14 @@ export const projects = [
       location: 'Thika',
       image: require('./images/1.jpg'),
       about: 'Description about Project 2.',
-      challenges: 'Challenges faced by Project 2.',
-      achievements: 'Achievements of Project 2.',
+      challenges: {
+        image: require('./images/challenge1.png'),
+        description: 'Challenges faced by Project 2.',
+      },
+      achievements: {
+        image: require('./images/achievement1.png'),
+        description: 'Achievements of Project 2.',
+      },
       partners: 'Partners involved in Project 2.',      
     },
     {
@@ -26,8 +38,14 @@ export const projects = [
       image: require('./images/6.png'),
       details: {
       about: 'Description about Project 3.',
-      challenges: 'Challenges faced by Project 3.',
-      achievements: 'Achievements of Project 3.',
+      challenges: {
+        image: require('./images/challenge1.png'),
+        description: 'Challenges faced by Project 3.',
+      },
+      achievements: {
+        image: require('./images/achievement1.png'),
+        description: 'Achievements of Project 3.',
+      },
       partners: 'Partners involved in Project 3.',
       },
     },
@@ -37,8 +55,14 @@ export const projects = [
       location: 'Kisumu',
       image: require('./images/2.jpg'),
       about: 'Description about Project 4.',
-      challenges: 'Challenges faced by Project 4.',
-      achievements: 'Achievements of Project 4.',
+      challenges: {
+        image: require('./images/challenge1.png'),
+        description: 'Challenges faced by Project 4.',
+      },
+      achievements: {
+        image: require('./images/achievement1.png'),
+        description: 'Achievements of Project 4.',
+      },
       partners: 'Partners involved in Project 4.',
     },
     {
@@ -47,8 +71,14 @@ export const projects = [
       location: 'Mombasa',
       image: require('./images/5.png'),
       about: 'Description about Project 5.',
-      challenges: 'Challenges faced by Project 5.',
-      achievements: 'Achievements of Project 5.',
+      challenges: {
+        image: require('./images/challenge1.png'),
+        description: 'Challenges faced by Project 5.',
+      },
+      achievements: {
+        image: require('./images/achievement1.png'),
+        description: 'Achievements of Project 5.',
+      },
       partners: 'Partners involved in Project 5.',
     },
     {
@@ -57,8 +87,14 @@ export const projects = [
       location: 'Ruiru',
       image: require('./images/6.png'),
       about: 'Description about Project 6.',
-      challenges: 'Challenges faced by Project 6.',
-      achievements: 'Achievements of Project 6.',
+      challenges: {
+        image: require('./images/challenge1.png'),
+        description: 'Challenges faced by Project 6.',
+      },
+      achievements: {
+        image: require('./images/achievement1.png'),
+        description: 'Achievements of Project 6.',
+      },
       partners: 'Partners involved in Project 6.',
     },
   ];
